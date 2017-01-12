06541c42b39fc13a6236e74e73577719f147d2d7
Alan Gibson <alan.gibson@gmail.com>
2017-01-12 17:34:20 +0000

    Fixing tmc26x shell

--- bin/tmc26x_shell.py
+++ bin/tmc26x_shell.py
@@ -1,14 +1,25 @@
 import asyncio, logging
-from RaspberryPiStepperDriver import spi, tmc26x
-from RaspberryPiStepperDriver.profiles import rectangle as rectangle_profile
+from RaspberryPiStepperDriver import drivers
+from RaspberryPiStepperDriver.activators import spi, tmc26x
+from RaspberryPiStepperDriver.profiles import accel as accel_profile, max as max_profile, rectangle as rectangle_profile
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-spi_dev = spi.SPI(0, 0)
-profile = rectangle_profile.RectangleProfile()
-driver = tmc26x.TMC26XStepper(spi_dev, profile,
-  dir_pin=23, step_pin=18, current=300, resistor=150)
+driver = drivers.StepperDriver(
+  activator=tmc26x.TMC26XActivator(
+    spi=spi.SPI(bus=0, device=0),
+    dir_pin=23,
+    step_pin=18,
+    current=300
+  ),
+  profile=rectangle_profile.RectangleProfile()
+)
+# stepper_driver.set_target_speed(stepper.max_speed)
+# stepper_driver.set_microsteps(1)
+# stepper_driver.set_current(1000)
+# stepper_driver.set_acceleration(40000) # steps per second per second
+# stepper_driver.set_pulse_width(2)
 
 driver.start()
 
